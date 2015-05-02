8806413896cdbabc0acd7ecaa1d5efb0573490fe
evx001 <eviexeris@gmail.com>
2015-05-02 03:51:38 -0400

    new

---
+++ elosnoc20150502/elosnoc20150502/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿module elosnoc20150502.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("elosnoc20150502")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("evxyz")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
+
